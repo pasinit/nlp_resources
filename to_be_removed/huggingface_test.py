82e575f8f7f6bcd3e083c0e6e2adee41e2c1bce9
Tommaso <p.tommaso@gmail.com>
2020-05-11 00:15:01 +0200

    pulizia contatti

--- to_be_removed/huggingface_test.py
+++
@@ -1,6 +0,0 @@
-from nlp_resources.nlp_models import GenericHuggingfaceWrapper
-
-if __name__ == "__main__":
-    model = GenericHuggingfaceWrapper("bert-base-cased", "cuda")
-    out = model(["this is a test"])
-    print(out)
--- to_be_removed/misc.py
+++
@@ -1,3 +0,0 @@
-from to_be_removed.bert_wrappers import BertTokeniserWrapper
-
-BertTokeniserWrapper("bert-base-cased", "cpu")
